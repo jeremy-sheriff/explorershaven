973ee32372da5cd79b74d143cacdeee3926a771f
muhohoweb <muhohoweb@icloud.com>
2026-01-02 02:36:30 +0300

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -7,27 +7,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FeePaymentController;
 use App\Http\Controllers\StudentController;
 
-Route::get('/',function (){
+Route::get('/', function () {
     return redirect('/login');
 })->name('home');
 
-
-
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+// Students routes - IMPORTANT: GET routes must come before PUT/DELETE routes
 Route::get('/students', [StudentController::class, 'index'])->name('students.index');
 Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show'); // MOVED BEFORE PUT/DELETE
 Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
-Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
 Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
+
 Route::get('/fees', [FeeController::class, 'index'])->name('fees.index');
+
 Route::get('/fee-payments', [FeePaymentController::class, 'index'])->name('fee-payments.index');
-Route::get('/fee-credits', [FeeCreditController::class, 'index'])->name('fee-credits.index');
 Route::post('/fee-payments', [FeePaymentController::class, 'store'])->name('fee-payments.store');
 Route::put('/fee-payments/{feePayment}', [FeePaymentController::class, 'update'])->name('fee-payments.update');
 Route::delete('/fee-payments/{feePayment}', [FeePaymentController::class, 'destroy'])->name('fee-payments.destroy');
 
+Route::get('/fee-credits', [FeeCreditController::class, 'index'])->name('fee-credits.index');
 
-
-
-
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
